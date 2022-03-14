4efc715bcf4fd3e25591c1c98826f2ee96339a6d
Andrea <onemanshow2099@gmail.com>
2022-03-14 16:21:18 +0100

    completato (grazie davide)

---
+++ script.js
@@ -0,0 +1,11 @@
+const nomeUtente = prompt("Inserisci il tuo nome")
+
+const cognomeUtente = prompt("Inserisci il tuo cognome")
+
+const colorePreferito = prompt("Inserisci il tuo colore preferito")
+
+console.log(`${nomeUtente + cognomeUtente + colorePreferito}21`)
+
+let password = `${nomeUtente + cognomeUtente + colorePreferito}21`
+
+document.write(password)
